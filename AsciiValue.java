43545820d974601a974e04d437ee21336c08c08f
snehamahalingam123 <84567458+snehamahalingam123@users.noreply.github.com>
2021-07-13 05:25:05 +1400

    initial commit

---
+++ AsciiValue.java
@@ -0,0 +1,15 @@
+package homewrk;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		char ch='H';
+		int ascii=ch;
+		
+		int castAscii=(int)ch;
+		System.out.println("the ASCII Value of "+ch+"is:"+ascii);
+		System.out.println("the ASCII Value of "+ch+"is:"+castAscii);
+
+	}
+
+}
